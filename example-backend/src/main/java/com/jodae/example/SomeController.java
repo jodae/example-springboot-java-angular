0c7b72b66de0df2b169c6c3f392f87c281a69a7e
Jodae <estela.jdave@gmail.com>
2022-02-24 01:08:50 +0100

    comment

--- example-backend/src/main/java/com/jodae/example/SomeController.java
+++ example-backend/src/main/java/com/jodae/example/SomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200") // we allow localhost:4200 for testing purposes with 'ng serve'
 public class SomeController {
 
 	@GetMapping(value = "/message")
